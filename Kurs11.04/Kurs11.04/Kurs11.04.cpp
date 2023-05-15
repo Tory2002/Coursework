9d89c835a99fe3dc15efaec706388bb8f720e795
Viktoryia <84477593+Tory2002@users.noreply.github.com>
2023-05-15 15:33:23 +0300

    Coursework

---
+++ Kurs11.04/Kurs11.04/Kurs11.04.cpp
@@ -0,0 +1,67 @@
+﻿// Kurs11.04.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+double f(double x) { // задаем функцию
+    
+    return pow(x, 4) - 16 * x - 64;
+}
+
+double df(double x, double h) { // вычисляем производную функции
+    return (f(x + h) - f(x - h)) / (2 * h);
+}
+
+double newton(double x, double eps, double h) { // алгоритм Ньютона
+    int iterations = 0; // счетчик итераций 
+    double curr_x = x; // текущее значение
+    double prev_x = x + 1; // предыдущее значение
+    double prev_f = f(prev_x); // значение функции на предыдущей итерации
+    while (abs(curr_x - prev_x) > eps) {// пока не достигнута заданная точность
+        prev_x = curr_x; //сохраняем текущее значение
+        double curr_f = f(prev_x); // вычисляем значение функции в текущей точке
+        double curr_df = df(prev_x, h);// вычисляем значение производной в текущей точке
+        if (abs(curr_df) < eps) { // проверка на сходимость
+            cout << "Ошибка: производная функции слишком близка к нулю." << endl;
+            return prev_x;
+        }
+        curr_x = prev_x - curr_f / curr_df;  // вычисляем следующее приближение
+        double ratio = abs(curr_f - prev_f) / abs(prev_f);
+        if (ratio < eps) { // проверка на сходимость
+            cout << "Алгоритм сошелся на итерации " << iterations << "." << endl;
+            break;
+        }
+        prev_f = curr_f;
+        iterations++;
+    }
+    cout << "Результат алгоритма Ньютона (итераций: " << iterations << "): ";
+    return curr_x;
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double x = 0; // начальное приближение
+    double eps = 0; // точность
+    double h = 0.0001; // шаг для численного дифференцирования
+    double result;
+    cout << "Введите начальное приближение: ";
+    cin >> x;
+    cout << "Введите точность: ";
+    cin >> eps;
+    if (eps == 0) { // проверка на ноль
+        cout << "Ошибка: введена нулевая точность." << endl;
+
+        return 0;
+    }
+    result = newton(x, eps, h); // вызываем алгоритм Ньютона
+    cout << result << endl;
+    return 0;
+    
+}
+
+
---
+++ Kurs11.04/KursTest1/KursTest1.cpp
@@ -0,0 +1,50 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Kurs11.04/Kurs11.04.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace KursTest1
+{
+	TEST_CLASS(KursTest1)
+	{
+	
+	public:
+		//Тест на проверку работы функции f(x):
+		TEST_METHOD(TestFunction)
+		{
+			double x = 4;
+			double expected = 128;
+			double actual = f(x);
+			Assert::AreEqual(expected, actual, 0.0001);
+		}
+		//Тест на проверку вычисления производной функции df(x, h) :
+
+		TEST_METHOD(TestDerivative)
+		{
+			double x = 4;
+			double h = 0.0001;
+			double expected = 240;
+			double actual = df(x, h);
+			Assert::AreEqual(expected, actual, 0.0001);
+		}
+
+		//Тест на проверку работы алгоритма Ньютона с заданными параметрами:
+
+		TEST_METHOD(TestNewton)
+		{
+			double x = 4;
+			double eps = 0.0001;
+			double h = 0.0001;
+			double expected = 3.28587;
+			double actual = newton(x, eps, h);
+			Assert::AreEqual(expected, actual, 0.0001);
+		}
+
+
+
+
+		
+		
+	};
+}
